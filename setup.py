966b1686c826c2d1986b1914e2e677326de5b880
slibanore <sarah.libanore@gmail.com>
2025-04-29 08:21:50 +0300

    setup installation

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+# SarahLibanore: install oLIMpus and zeus21
+setup(
+    name='oLIMpus',
+          version='0.1dev',
+          description='oLIMpus: cross-correlating lines with Zeus21.',
+          url='https://github.com/slibanore/Zeus21',
+          author='Sarah Libanore, Julian B. Muñoz, Ely Kovetz, Yonatan Sklansky, Hector Afonso G. Cruz',
+          author_email='libanore@bgu.ac.il',
+          #license='MIT',
+          packages=find_packages(),
+          long_description=open('README.md').read(),
+          install_requires=[
+           "mcfit",
+           "classy",
+           "numexpr",
+           "astropy",
+           "pyfftw",
+           "powerbox",
+           "tqdm",
+           "matplotlib"
+       ],
+)
