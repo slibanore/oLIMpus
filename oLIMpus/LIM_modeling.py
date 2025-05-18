fixed normalization, added CO and CII

oLIMpus/LIM_modeling.py
@@ -78,23 +78,26 @@ class get_LIM_coefficients:
         modd_LIM = Cosmo_Parameters.delta_crit_ST - deltaArray_LIM
         nu = modd_LIM / modSigmaSq_LIM # used in the HMF
 
-        EPS_HMF_corr = (nu/nu0) * (sigmaM_LIM/modSigmaSq_LIM)**2.0 * np.exp(-Cosmo_Parameters.a_corr_EPS * (nu**2-nu0**2)/2.0 ) 
+        EPS_HMF_corr_Lag = (nu/nu0) * (sigmaM_LIM/modSigmaSq_LIM)**2.0 * np.exp(-Cosmo_Parameters.a_corr_EPS * (nu**2-nu0**2)/2.0 ) 
+
+        EPS_HMF_corr = (1.0 + deltaArray_LIM) * EPS_HMF_corr_Lag
 
         # ---- #
-        # move to eulerian space if required
-        # !!! change to User_Params
-        if Line_Parameters.Eulerian:
-            EPS_HMF_corr *= (1.0 + deltaArray_LIM)
+        # get the correct mean accounting for EPS 
+        integrand_LIM_Lag = EPS_HMF_corr_Lag * rhoL_integrand(False, Line_Parameters,Astro_Parameters, Cosmo_Parameters, HMF_interpolator, mArray_LIM, zArray_LIM)
+        self.rhoL_dR_Lag = np.trapz(integrand_LIM_Lag, HMF_interpolator.logtabMh, axis = 1)
 
-        # ---- #
         # get the correct mean accounting for EPS and Eulerian
         integrand_LIM = EPS_HMF_corr * rhoL_integrand(False, Line_Parameters,Astro_Parameters, Cosmo_Parameters, HMF_interpolator, mArray_LIM, zArray_LIM)
-
         self.rhoL_dR = np.trapz(integrand_LIM, HMF_interpolator.logtabMh, axis = 1)
+
 
         # ---- #
         # compute the gammas for the lognormal approximation as the derivatives of rhoL in Eulerian space
         midpoint = deltaArray_LIM.shape[-1]//2 
+
+        self.gamma_LIM_Lag = np.log(self.rhoL_dR_Lag[:,midpoint+1]/self.rhoL_dR_Lag[:,midpoint-1]) / (deltaArray_LIM[:,0,midpoint+1] - deltaArray_LIM[:,0,midpoint-1])
+        self.gamma_LIM_Lag[np.isnan(self.gamma_LIM_Lag)] = 0.0            
 
         self.gamma_LIM = np.log(self.rhoL_dR[:,midpoint+1]/self.rhoL_dR[:,midpoint-1]) / (deltaArray_LIM[:,0,midpoint+1] - deltaArray_LIM[:,0,midpoint-1])
         self.gamma_LIM[np.isnan(self.gamma_LIM)] = 0.0            
@@ -106,22 +109,41 @@ class get_LIM_coefficients:
         y0 = np.log(self.rhoL_dR[:,midpoint-1])
         y1 = np.log(self.rhoL_dR[:,midpoint])
         y2 = np.log(self.rhoL_dR[:,midpoint+1])
-
         self.gamma2_LIM = 2.*(y0/((x1-x0)*(x2-x0)) + y2/((x2-x1)*(x2-x0)) - y1/((x2-x1)*(x1-x0))) / 2.
         self.gamma2_LIM[np.isnan(self.gamma2_LIM)] = 0.0
 
+        if Line_Parameters.quadratic_lognormal:
+            self.norm_exp = np.exp((self.gamma_LIM * self.sigmaofRtab_LIM)**2/(2-4*self.gamma2_LIM*self.sigmaofRtab_LIM**2)) / np.sqrt(1-2*self.gamma2_LIM*self.sigmaofRtab_LIM**2)
+        else:
+            self.norm_exp = np.exp((self.gamma_LIM * self.sigmaofRtab_LIM)**2/2)
+
     ### STEP 3: Correct Eulerian-Lagrangian mean
-        if(User_Parameters.C2_RENORMALIZATION_FLAG==True and Line_Parameters.Eulerian==True): # !!! move this flag to User_Params
+        if(User_Parameters.C2_RENORMALIZATION_FLAG==True): # !!! move this flag to User_Params
 
-            gamma_LIM_Lagrangian = self.gamma_LIM-1.0
+            sigma = self.sigmaofRtab_LIM**2
+
+            gamma_Lag = self.gamma_LIM_Lag
+            #gamma_Lag = self.gamma_LIM -1
+
+            #gamma_LIM_Lagrangian = self.gamma_LIM-1.0
             if Line_Parameters.quadratic_lognormal: # !!! move this flag to User_Params
-                gamma2_LIM_Lagrangian = self.gamma2_LIM + 1/2.
+                y0_Lag = np.log(self.rhoL_dR_Lag[:,midpoint-1])
+                y1_Lag = np.log(self.rhoL_dR_Lag[:,midpoint])
+                y2_Lag = np.log(self.rhoL_dR_Lag[:,midpoint+1])
+                self.gamma2_LIM_Lag = 2.*(y0_Lag/((x1-x0)*(x2-x0)) + y2_Lag/((x2-x1)*(x2-x0)) - y1_Lag/((x2-x1)*(x1-x0))) / 2.
+                self.gamma2_LIM_Lag[np.isnan(self.gamma2_LIM_Lag)] = 0.0
 
-                _corrfactorEulerian_LIM = (1.+(gamma_LIM_Lagrangian-2*gamma2_LIM_Lagrangian)*self.sigmaofRtab_LIM**2)/(1.-2*gamma2_LIM_Lagrangian*self.sigmaofRtab_LIM**2)
+                gamma2_Lag = self.gamma2_LIM_Lag
+                #gamma2_Lag = self.gamma2_LIM -1
+
+                _corrfactorEulerian_LIM = (1+(gamma_Lag-2*gamma2_Lag)*sigma**2)/(1-2*gamma2_Lag*sigma**2) 
+                #_corrfactorEulerian_LIM = self.norm_exp / (np.exp((self.gamma_LIM_Lag * self.sigmaofRtab_LIM)**2/(2-4*self.gamma2_LIM_Lag*self.sigmaofRtab_LIM**2)) / np.sqrt(1-2*self.gamma2_LIM_Lag*self.sigmaofRtab_LIM**2))
+                
 
             else:
-                _corrfactorEulerian_LIM = 1.0 + gamma_LIM_Lagrangian*self.sigmaofRtab_LIM**2
-
+                _corrfactorEulerian_LIM =  1+ gamma_Lag * sigma**2
+                #_corrfactorEulerian_LIM = self.norm_exp / np.exp((self.gamma_LIM_Lag * self.sigmaofRtab_LIM)**2/2)
+                
             self.rhoL_bar *= _corrfactorEulerian_LIM
 
     ### STEP 4: Line Intensity Anisotropies
@@ -158,8 +180,7 @@ class get_LIM_coefficients:
             
             self.shot_noise = scale_power_spectrum.value * np.trapezoid(integrand_shot, HMF_interpolator.logtabMh, axis = 1) 
 
-            if Line_Parameters.Eulerian:
-                self.shot_noise *= _corrfactorEulerian_LIM**2
+            self.shot_noise *= _corrfactorEulerian_LIM**2
 
 
 ### ---------------------- ###
@@ -276,7 +297,80 @@ def LineLuminosity(dotM, Line_Parameters, Astro_Parameters, Cosmo_Parameters, HM
         log10_L[cond2] = a + (ma - mb) * log10_SFR_b + mb * log10_SFR[cond2]
         log10_L[cond3] = a + (ma - mb) * log10_SFR_b + (mb - mc) * log10_SFR_c + mc * log10_SFR[cond3]
 
+    elif Line_Parameters.LINE_MODEL == 'Lagache18':
 
+        if Line_Parameters.LINE != 'CII':
+            print('\nLINE NOT IMPLEMENTED YET IN THESAN21')
+            return -1
+
+        line_dict = inputs_LIM.Lagache18_CII_params
+
+        alpha_SFR =line_dict['alpha_SFR_0'] + line_dict['alpha_SFR'] * z
+
+        beta_SFR = line_dict['beta_SFR_0'] + line_dict['beta_SFR'] * z
+
+        try:
+            alpha_SFR[alpha_SFR < 0.] = 0. 
+        except:
+            if alpha_SFR < 0.:
+                alpha_SFR = 0.
+
+        log10_L = alpha_SFR * log10_SFR + beta_SFR     
+
+    elif Line_Parameters.LINE_MODEL == 'Yang21':
+
+        if Line_Parameters.LINE == 'CO21':
+            line_dict = inputs_LIM.Yang21_CO21_params
+            if z<4.0:
+                logM1 = YangEmp_f2(12.12,-.1704,0,z)
+                logN = YangEmp_f2(-5.95,.278,-.0521,z)
+                a = YangEmp_f2(1.69,.126,-.028,z)
+                b = YangEmp_f1(1.8,2.76,-.0678,z)
+            elif z<5.0:
+                logM1 = YangEmp_f2(11.74,-.07050,0,z)
+                logN = YangEmp_f2(-5.57,-.025,0,z)
+                a = YangEmp_f2(4.557,-1.215,.13,z)
+                b = YangEmp_f2(.657,-.0794,0,z)
+            #elif z<8.5:
+            else:
+                logM1 = YangEmp_f2(11.63,-.05266,0,z)
+                logN = YangEmp_f2(-5.26,-.0849,0,z)
+                a = YangEmp_f2(2.47,-.210,.0132,z)
+                b = YangEmp_f1(38.3,.841,.169,z)
+
+        else:
+            print('\nLINE NOT IMPLEMENTED YET IN YANG21')
+            return -1
+
+        # Empirically fit parameter values for the Yang+ empirical XX model
+        A = line_dict['A']
+
+        YangEmp_f2 = lambda x1, x2, x3, zz: 1 + x2*z + x3*zz**2
+        YangEmp_f1 = lambda x1, x2, x3, zz: x1*np.exp(-zz/x2) + x3
+        
+        M1 = 10**logM1
+        N = 10**logN
+
+        log10_L = np.log10(A*(2*N*massVector/((massVector/M1)**-a+(massVector/M1)**b)))
+
+    elif Line_Parameters.LINE_MODEL == 'Li16':
+
+        if Line_Parameters.LINE == 'CO21':
+            line_dict = inputs_LIM.Li16_C021_params
+        else:
+            print('\nLINE NOT IMPLEMENTED YET IN YANG21')
+            return -1
+
+        alpha = line_dict['alpha']
+        beta = line_dict['beta']
+        dMF = line_dict['dMF']
+        L0 = line_dict['L0']
+
+        L_IR = 10**log10_SFR / (dMF*1e-10)
+        Lprime = (10.**-beta * L_IR)**(1./alpha)
+    
+        log10_L = np.log10(L0*Lprime)
+    
     else:
         print('\nLINE MODEL NOT IMPLEMENTED YET')
         return -1

oLIMpus/correlations_LIM.py
@@ -84,7 +84,8 @@ class Power_Spectra_LIM:
         # in this case we have a cross power spectrum between lines and no cross with density
             self._Pk_LIM_lin = (self.window_LIM.T * self.window_LIM_cross.T * self.lin_growth**2)[:,np.newaxis] * (Window(LIM_corr.WINDOWTYPE, LIM_corr._klistCF, Line_Parameters._R)* (Window(LIM_corr.WINDOWTYPE, LIM_corr._klistCF, Line_Parameters_cross._R)) * LIM_corr._PklinCF)[np.newaxis,:]
 
-            self._Pk_deltaLIM_lin = np.zeros_like(self._Pk_LIM_lin)
+            self._Pk_deltaLIM_lin = (self.window_LIM.T * self.lin_growth**2)[:,np.newaxis] * (Window(LIM_corr.WINDOWTYPE, LIM_corr._klistCF, Line_Parameters._R) *  LIM_corr._PklinCF)[np.newaxis,:] 
+            self._Pk_deltaLIM_lin_cross = (self.window_LIM_cross.T * self.lin_growth**2)[:,np.newaxis] * (Window(LIM_corr.WINDOWTYPE, LIM_corr._klistCF, Line_Parameters_cross._R) *  LIM_corr._PklinCF)[np.newaxis,:] 
 
         self.Deltasq_LIM_lin = self._Pk_LIM_lin * self._k3over2pi2                                                                 
         self.Deltasq_deltaLIM_lin = self._Pk_deltaLIM_lin * self._k3over2pi2 
@@ -106,7 +107,8 @@ class Power_Spectra_LIM:
         if (Line_Parameters._R < User_Parameters.MAX_R_NONLINEAR and Line_Parameters_cross is None): #User_Parameters.FLAG_DO_DENS_NL and 
             self._Pk_deltaLIM = self.get_list_PS(self._xiR1_deltaLIM, LIM_coeff.zintegral)
         else:
-            self._Pk_deltaLIM = self._Pk_deltaLIM_lin
+            self._Pk_deltaLIM = self.get_list_PS(self._xiR1_deltaLIM, LIM_coeff.zintegral)
+            self._Pk_deltaLIM_cross = self.get_list_PS(self._xiR1_deltaLIM_cross, LIM_coeff.zintegral)
         
     ### STEP 5: add RSD             
         if(self.RSD_MODE==0): #spherically avg'd RSD
@@ -122,7 +124,11 @@ class Power_Spectra_LIM:
         # f(z) = dln D(d)/dln a = dln D(z) / dz * (dz/dln a)
         growth_rate = - (1.+LIM_coeff.zintegral) * (np.log(cosmology.growth(Cosmo_Parameters, LIM_coeff.zintegral+dzlist))-np.log(cosmology.growth(Cosmo_Parameters, LIM_coeff.zintegral-dzlist)))/(2.0*dzlist) 
 
-        self._Pk_LIM_RSD = self._Pk_LIM + LIM_coeff.Inu_bar[:,np.newaxis]**2 * (growth_rate[:,np.newaxis] * mu2 * self.lin_growth[:,np.newaxis])**2 * LIM_corr._PklinCF[np.newaxis,:] + 2 * LIM_coeff.Inu_bar[:,np.newaxis] * growth_rate[:,np.newaxis] * mu2 * self._Pk_deltaLIM
+        if Line_Parameters_cross is None:
+            self._Pk_LIM_RSD = self._Pk_LIM + LIM_coeff.Inu_bar[:,np.newaxis]**2 * (growth_rate[:,np.newaxis] * mu2 * self.lin_growth[:,np.newaxis])**2 * LIM_corr._PklinCF[np.newaxis,:] + 2 * LIM_coeff.Inu_bar[:,np.newaxis] * growth_rate[:,np.newaxis] * mu2 * self._Pk_deltaLIM
+        else:
+            self._Pk_LIM_RSD = self._Pk_LIM + LIM_coeff.Inu_bar[:,np.newaxis]*LIM_coeff_cross.Inu_bar[:,np.newaxis] * (growth_rate[:,np.newaxis] * mu2 * self.lin_growth[:,np.newaxis])**2 * LIM_corr._PklinCF[np.newaxis,:] + LIM_coeff.Inu_bar[:,np.newaxis] * growth_rate[:,np.newaxis] * mu2 * self._Pk_deltaLIM + LIM_coeff_cross.Inu_bar[:,np.newaxis] * growth_rate[:,np.newaxis] * mu2 * self._Pk_deltaLIM_cross
+
 
     ### STEP 6: shot noise
         if Line_Parameters.shot_noise and Line_Parameters_cross is None:
@@ -183,28 +189,33 @@ class Power_Spectra_LIM:
                 gammaR2_NL = LIM_coeff_cross.gamma2_LIM[:,np.newaxis]
                 g2NL = gammaR2_NL * sigmaR2**2
 
+            norm1 = LIM_coeff.norm_exp[:,np.newaxis] #ne.evaluate('exp(g1 * g1 / (2 - 4 * g1NL)) / sqrt(1 - 2 * g1NL)') 
+            if Line_Parameters_cross is None:
+                norm2 = LIM_coeff.norm_exp[:,np.newaxis] #ne.evaluate('exp(g2 * g2 / (2 - 4 * g2NL)) / sqrt(1 - 2 * g2NL)') 
+            else:
+                norm2 = LIM_coeff_cross.norm_exp[:,np.newaxis]
+
+
             numerator_NL = ne.evaluate('xi_LIM_R1R2_z + g1 * g1 * (0.5 - g2NL * (1 - xi_matter_R1R2_z * xi_matter_R1R2_z)) + g2 * g2 * (0.5 - g1NL * (1 - xi_matter_R1R2_z * xi_matter_R1R2_z))')
             
             denominator_NL = ne.evaluate('1. - 2 * g1NL - 2 * g2NL + 4 * g1NL * g2NL * (1 - xi_matter_R1R2_z * xi_matter_R1R2_z)')
-            
-            norm1 = ne.evaluate('exp(g1 * g1 / (2 - 4 * g1NL)) / sqrt(1 - 2 * g1NL)') 
-            norm2 = ne.evaluate('exp(g2 * g2 / (2 - 4 * g2NL)) / sqrt(1 - 2 * g2NL)') 
             
             log_norm = ne.evaluate('log(sqrt(denominator_NL) * norm1 * norm2)')
 
             nonlinearcorrelation = ne.evaluate('exp(numerator_NL/denominator_NL - log_norm)-1')
 
         else:
-            nonlinearcorrelation = ne.evaluate('exp(xi_LIM_R1R2_z)-1')
+            nonlinearcorrelation = ne.evaluate('(exp(xi_LIM_R1R2_z)-1)')
 
         if Line_Parameters_cross is None:
-            self._xiR1R2_LIM = LIM_coeff.Inu_bar[:,np.newaxis]**2 * nonlinearcorrelation
+            self._xiR1R2_LIM = LIM_coeff.Inu_bar[:,np.newaxis]**2 * nonlinearcorrelation 
+
         else:
             self._xiR1R2_LIM = (LIM_coeff.Inu_bar * LIM_coeff_cross.Inu_bar)[:,np.newaxis] * nonlinearcorrelation
 
         # --- #
         # if also matter treated as a smoothed lognormal
-        if (Line_Parameters._R < User_Parameters.MAX_R_NONLINEAR and Line_Parameters_cross is None): # User_Parameters.FLAG_DO_DENS_NL and
+        if (Line_Parameters._R < User_Parameters.MAX_R_NONLINEAR ): # User_Parameters.FLAG_DO_DENS_NL and
 
             windowR1 = Window(LIM_corr.WINDOWTYPE, LIM_corr._klistCF, Line_Parameters._R) # only one value for the resolution but defined for array on the ks
             _Pksmooth = np.array(LIM_corr._PklinCF) * windowR1
@@ -218,19 +229,44 @@ class Power_Spectra_LIM:
                 numerator_NL = ne.evaluate('gammaR1 * xi_matter_R10_z + gammaR1_NL * xi_matter_R10_z*xi_matter_R10_z + g1*g1/2')
                     
                 denominator_NL = ne.evaluate('1. - 2 * g1NL')
-                
-                norm1 = ne.evaluate('exp(g1 * g1 / (2 - 4 * g1NL)) / sqrt(1 - 2 * g1NL)') 
-                
+                                
                 log_norm = ne.evaluate('log(sqrt(denominator_NL) * norm1)')
                 
                 nonlinear_deltaLIM = ne.evaluate('exp(numerator_NL/denominator_NL - log_norm)-1')
 
             else:
-                nonlinear_deltaLIM = ne.evaluate('exp(gammaR1*xi_matter_R10_z)-1')
+                nonlinear_deltaLIM = ne.evaluate('(exp(gammaR1*xi_matter_R10_z) -1)')
 
             self._xiR1_deltaLIM = LIM_coeff.Inu_bar[:,np.newaxis] * nonlinear_deltaLIM
+
+            if Line_Parameters_cross is not None:
+
+                windowR2_cross = Window(LIM_corr.WINDOWTYPE, LIM_corr._klistCF, Line_Parameters_cross._R) # only one value for the resolution but defined for array on the ks
+                _Pksmooth_cross = np.array(LIM_corr._PklinCF) * windowR2_cross
+
+                self.rlist_CF, xi_R20_CF_cross = LIM_corr._xif(_Pksmooth, extrap = False) 
+                xi_matter_R20_z0_cross = (xi_R20_CF_cross)[np.newaxis,:]
+                xi_matter_R20_z_cross = ne.evaluate('xi_matter_R20_z0_cross * growthRmatrix')
+         
+                if Line_Parameters.quadratic_lognormal:
+
+                    numerator_NL = ne.evaluate('gammaR2 * xi_matter_R20_z_cross + gammaR2_NL * xi_matter_R20_z_cross*xi_matter_R20_z_cross + g2*g2/2')
+                        
+                    denominator_NL = ne.evaluate('1. - 2 * g2NL')
+                                        
+                    log_norm = ne.evaluate('log(sqrt(denominator_NL) * norm2)')
+                    
+                    nonlinear_deltaLIM = ne.evaluate('exp(numerator_NL/denominator_NL - log_norm)-1')
+
+                else:
+                    nonlinear_deltaLIM = ne.evaluate('(exp(gammaR*xi_matter_R20_z_cross)-1)')
+
+                self._xiR1_deltaLIM_cross = LIM_coeff_cross.Inu_bar[:,np.newaxis] * nonlinear_deltaLIM
+
         else:
             self._xiR1_deltaLIM = 0.
+            if Line_Parameters_cross is not None:
+                self._xiR1_deltaLIM_cross = 0.
 
 
     # --- #

oLIMpus/maps_LIM.py
@@ -161,10 +161,7 @@ class CoevalBox_percell:
         mean_SFRD_numerical = np.mean(SFRDbox_flattend)
         SFRDbox_Lagrangian_flattened = ne.evaluate('mean_SFRD_theory/mean_SFRD_numerical * SFRDbox_flattend')
 
-        if Line_Parameters.Eulerian: # !!! move to user_params
-            SFRDbox_flattend_scaled = ne.evaluate('SFRDbox_Lagrangian_flattened * (1+density_box)')
-        else:
-            SFRDbox_flattend_scaled = SFRDbox_Lagrangian_flattened
+        SFRDbox_flattend_scaled = ne.evaluate('SFRDbox_Lagrangian_flattened * (1+density_box)')
 
         self.SFRD_box = SFRDbox_flattend_scaled.reshape(Nbox,Nbox,Nbox)
 
@@ -175,14 +172,12 @@ class CoevalBox_percell:
         rhoLbox_flattened = np.trapezoid(integrand_LIM, HMF_interpolator.logtabMh, axis = 0) 
 
         # scale the mean 
-        mean_rhoL_theory = LIM_coeff.rhoL_avg[_iz] # this is in Lagrangian
-        mean_rhoL_numerical = np.mean(rhoLbox_flattened)
-        rhoLbox_Lagrangian_flattened = ne.evaluate('mean_rhoL_theory/mean_rhoL_numerical * rhoLbox_flattened')
+        # mean_rhoL_theory = LIM_coeff.rhoL_avg[_iz] # this is in Lagrangian
+        # mean_rhoL_numerical = np.mean(rhoLbox_flattened)
+        # rhoLbox_Lagrangian_flattened = ne.evaluate('mean_rhoL_theory/mean_rhoL_numerical * rhoLbox_flattened')
+        rhoLbox_Lagrangian_flattened = ne.evaluate('rhoLbox_flattened')
          
-        if Line_Parameters.Eulerian:
-            rhoLbox_flattend_scaled = ne.evaluate('rhoLbox_Lagrangian_flattened * (1+density_box)')
-        else:
-            rhoLbox_flattend_scaled = rhoLbox_Lagrangian_flattened
+        rhoLbox_flattend_scaled = ne.evaluate('rhoLbox_Lagrangian_flattened * (1+density_box)')
 
         self.rhoL_box = rhoLbox_flattend_scaled.reshape(Nbox,Nbox,Nbox)
 