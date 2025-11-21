fixed computation with small resolution

oLIMpus/maps_LIM.py
@@ -331,12 +331,14 @@ class CoevalMaps_T21noreionization:
         Dsq_T21 = Power_Spectrum.Deltasq_T21[_iz]
 
         Pd = Power_Spectrum.Deltasq_d_lin[_iz,:]/k3over2pi2
-        Pdinterp = interp1d(klist,Pd,fill_value=0.0,bounds_error=False)
+        #Pdinterp = interp1d(klist,Pd,fill_value=0.0,bounds_error=False)
+        Pd_spl = spline(np.log(klist), np.log(Pd))
 
         pb = pbox.PowerBox(
             N=self.Nbox,                     
             dim=3,                     
-            pk = lambda k: Pdinterp(k), 
+            #pk = lambda k: Pdinterp(k), 
+            pk = lambda k: np.exp(Pd_spl(np.log(k))), 
             boxlength = self.Lbox,           
             seed = self.seed               
         )
@@ -346,12 +348,13 @@ class CoevalMaps_T21noreionization:
         #then we make a map of the linear T21 fluctuation, better to use the cross to keep sign, at linear level same 
         PdT21 = Dsq_dT21/k3over2pi2
 
-        powerratioint = interp1d(klist,PdT21/Pd,fill_value=0.0,bounds_error=False)
+        #powerratioint = interp1d(klist,PdT21/Pd,fill_value=0.0,bounds_error=False)
+        powerratio_spl = spline(klist, PdT21/Pd) #cross can be negative, so can't interpolate over log values
 
 
         deltak = pb.delta_k()
 
-        powerratio = powerratioint(pb.k())
+        powerratio = powerratio_spl(pb.k())
         T21lin_k = powerratio * deltak
         self.T21maplin= self.T21global_noR + z21_utilities.powerboxCtoR(pb,mapkin = T21lin_k)
 

oLIMpus/zeus21_local/zeus21/maps.py
@@ -46,13 +46,14 @@ class CoevalMaps:
         if (KIND == 0): #just T21, ~gaussian
                 
             P21 = Power_Spectrum.Deltasq_T21_lin[_iz]/k3over2pi2
-            P21norminterp = interp1d(klist,P21/self.T21global_noR**2,fill_value=0.0,bounds_error=False)
+            # P21norminterp = interp1d(klist,P21/self.T21global_noR**2,fill_value=0.0,bounds_error=False)
+            P21_spl = spline(np.log(klist), np.log(P21/self.T21global**2)) #spline over log values
 
 
             pb = pbox.PowerBox(
                 N=self.Nbox,                     
                 dim=3,                     
-                pk = lambda k: P21norminterp(k), 
+                pk = lambda k: np.exp(P21_spl(np.log(k))), 
                 boxlength = self.Lbox,           
                 seed = self.seed                
             )
@@ -64,12 +65,14 @@ class CoevalMaps:
             
         elif (KIND == 1):
             Pd = Power_Spectrum.Deltasq_d_lin[_iz,:]/k3over2pi2
-            Pdinterp = interp1d(klist,Pd,fill_value=0.0,bounds_error=False)
+            #Pdinterp = interp1d(klist,Pd,fill_value=0.0,bounds_error=False)
+            Pd_spl = spline(np.log(klist), np.log(Pd))
 
             pb = pbox.PowerBox(
                 N=self.Nbox,                     
                 dim=3,                     
-                pk = lambda k: Pdinterp(k), 
+                #pk = lambda k: Pdinterp(k), 
+                pk = lambda k: np.exp(Pd_spl(np.log(k))), 
                 boxlength = self.Lbox,           
                 seed = self.seed               
             )
@@ -79,12 +82,13 @@ class CoevalMaps:
             #then we make a map of the linear T21 fluctuation, better to use the cross to keep sign, at linear level same 
             PdT21 = Power_Spectrum.Deltasq_dT21[_iz]/k3over2pi2
 
-            powerratioint = interp1d(klist,PdT21/Pd,fill_value=0.0,bounds_error=False)
+            # powerratioint = interp1d(klist,PdT21/Pd,fill_value=0.0,bounds_error=False)
+            powerratio_spl = spline(klist, PdT21/Pd) #cross can be negative, so can't interpolate over log values
 
 
             deltak = pb.delta_k()
 
-            powerratio = powerratioint(pb.k())
+            powerratio = powerratio_spl(pb.k())
             T21lin_k = powerratio * deltak
             self.T21maplin= self.T21global + z21_utilities.powerboxCtoR(pb,mapkin = T21lin_k)
 
@@ -407,9 +411,10 @@ class reionization_maps:
 
         iterator = trange(len(self.z)) if self.PRINT_TIMER else range(len(self.z))
         for i in iterator:
-            nion_spl = spline(sample_d, BMF.nion_delta_r_int(CosmoParams, sample_d, self.z, r)[:, i])
-            nrec_spl = spline(sample_d, BMF.nrec(CosmoParams, sample_d, BMF.ion_frac, self.z)[:, i])
-            partial_ion_spl = spline(sample_d, nion_spl(sample_d)/(1+nrec_spl(sample_d)))
+            # nion_spl = spline(sample_d, BMF.nion_delta_r_int(CosmoParams, sample_d, self.z, r)[:, i])
+            # nrec_spl = spline(sample_d, BMF.nrec(CosmoParams, sample_d, BMF.ion_frac, self.z)[:, i])
+            # partial_ion_spl = spline(sample_d, nion_spl(sample_d)/(1+nrec_spl(sample_d)))
+            partial_ion_spl = spline(sample_d, BMF.prebarrier_xHII_int_grid(sample_d, self.z[i], r)) #spline is faster than RGI, so build a spline on sample densities
 
             #if need to do by slices:
             #np.array([partial_ion_noclip(maps.density_allz[:, i], maps.ion_field_allz[:, i], ir=0) for i in trange(300)]).transpose(1, 0, 2, 3)

oLIMpus/zeus21_local/zeus21/reionization.py
@@ -31,6 +31,7 @@ class BMF:
         self.zlist = CoeffStructure.zintegral
         self.Rs = CoeffStructure.Rtabsmoo
         self.Rs_BMF = np.logspace(np.log10(Rmin), np.log10(self.Rs[-1]), 100)
+        self.ds_array = np.linspace(-1, 5, 101)
 
         
         self.gamma = CoeffStructure.gamma_niondot_II_index2D
@@ -60,9 +61,13 @@ class BMF:
         self.nion_norm = self.nion_normalization(zr_mesh[1], zr_mesh[0])
         self.nion_norm_int = RegularGridInterpolator(self.zr, self.nion_norm, bounds_error = False, fill_value = np.nan)
         
+        self.prebarrier_xHII = np.empty((len(self.ds_array), len(self.zlist), len(self.Rs)))
         self.barrier = self.compute_barrier(CosmoParams, self.ion_frac, self.zlist, self.Rs)
         self.barrier_initial = np.copy(self.barrier)
         self.barrier_int = RegularGridInterpolator(self.zr, self.barrier, bounds_error = False, fill_value = np.nan)
+
+        self.dzr = [self.ds_array, self.zlist, np.log(self.Rs)]
+        self.prebarrier_xHII_int = RegularGridInterpolator(self.dzr, self.prebarrier_xHII, bounds_error = False, fill_value = None) #allow extrapolation
 
         self.R_linear_sigma_fit_idx = z21_utilities.find_nearest_idx(self.Rs, R_linear_sigma_fit_input)
         self.R_linear_sigma_fit = self.Rs[self.R_linear_sigma_fit_idx]
@@ -75,8 +80,17 @@ class BMF:
         if FLAG_converge:
             self.converge_BMF(CosmoParams, self.ion_frac, max_iter=max_iter)
         #two functions: compute BMF and iterate
-        
 
+    def compute_prebarrier_xHII(self, CosmoParams, ion_frac, z, R):
+        """
+        
+        """
+        nion_values = self.nion_delta_r_int(CosmoParams, z, R)  #Shape (nd, nz)
+        nrec_values = self.nrec(CosmoParams, ion_frac, z)       #Shape (nd, nz)
+        
+        prebarrier_xHII = nion_values / (1 + nrec_values)
+
+        return prebarrier_xHII
 
     def compute_barrier(self, CosmoParams, ion_frac, z, R):
         """
@@ -105,28 +119,30 @@ class BMF:
         
         for ir in range(len(R)):
             #Compute nion_values and nrec_values for this 'ir'
-            nion_values = self.nion_delta_r_int(CosmoParams, ds_array, z, R[ir])  #Shape (nd, nz)
-            nrec_values = self.nrec(CosmoParams, ds_array, ion_frac, z)             #Shape (nd, nz)
-        
-            total_values = np.log10(nion_values / (1 + nrec_values) + 1e-10)   #taking difference in logspace to find zero-crossing 
-        
+            # nion_values = self.nion_delta_r_int(CosmoParams, ds_array, z, R[ir])  #Shape (nd, nz)
+            # nrec_values = self.nrec(CosmoParams, ds_array, ion_frac, z)             #Shape (nd, nz)
+            # total_values = np.log10(nion_values / (1 + nrec_values) + 1e-10)   #taking difference in logspace to find zero-crossing 
+
+            self.prebarrier_xHII[:, :, ir] =  self.compute_prebarrier_xHII(CosmoParams, ion_frac, z, R[ir])
+            total_values = np.log10(self.prebarrier_xHII[:, :, ir] + 1e-10)
+
             #Loop over redshift indices
-            for i in range(len(self.zlist)):
-                y_values = total_values[:, i]  #Shape (nd,)
+            for iz in range(len(self.zlist)):
+                y_values = total_values[:, iz]  #Shape (nd,)
         
                 #Find zero crossings
                 sign_change = np.diff(np.sign(y_values))
                 idx = np.where(sign_change)[0]
                 if idx.size > 0:
                     #Linear interpolation to find zero crossings
-                    x0 = ds_array[idx]
-                    x1 = ds_array[idx + 1]
+                    x0 = self.ds_array[idx]
+                    x1 = self.ds_array[idx + 1]
                     y0 = y_values[idx]
                     y1 = y_values[idx + 1]
                     x_intersect = x0 - y0 * (x1 - x0) / (y1 - y0)
-                    barrier[i, ir] = x_intersect[0]  #Assuming we take the first crossing
+                    barrier[iz, ir] = x_intersect[0]  #Assuming we take the first crossing
                 else:
-                    barrier[i, ir] = np.nan #Never crosses
+                    barrier[iz, ir] = np.nan #Never crosses
         barrier = barrier * (CosmoParams.growthint(self.zlist)/CosmoParams.growthint(self.zlist[0]))[:, np.newaxis] #scale barrier with growth factor
         barrier[self.zlist > self.ZMAX_REION] = 100 #sets density to an unreachable barrier, as if reionization isn't happening
         return barrier
@@ -135,7 +151,7 @@ class BMF:
     def nion_normalization(self, z, R):
         return 1/np.sqrt(1-2*self.gamma2[z, R]*self.sigma[z, R]**2)*np.exp(self.gamma[z, R]**2 * self.sigma[z, R]**2 / (2-4*self.gamma2[z, R]*self.sigma[z, R]**2))
 
-    def nrec(self, CosmoParams, d_array, ion_frac, z):
+    def nrec(self, CosmoParams, ion_frac, z, d_array=None):
         """
         Vectorized computation of nrec over an array of overdensities d_array.
 
@@ -156,7 +172,10 @@ class BMF:
         zarg = np.argsort(z) #sort just in case
         z = z[zarg]
         ion_frac = ion_frac[zarg]
-        
+
+        if d_array is None:
+            d_array = self.ds_array
+
         #reverse the inputs to make the integral easier to compute
         z_rev = z[::-1]
         Hz_rev = cosmology.Hubinvyr(CosmoParams, z_rev)
@@ -174,7 +193,7 @@ class BMF:
         nrecs = nrecs[:, ::-1]  #reverse back to increasing z order
         return nrecs
     
-    def niondot_delta_r(self, CosmoParams, d_array, z, R):
+    def niondot_delta_r(self, CosmoParams, z, R, d_array=None):
         """
         Compute niondot over an array of overdensities d_array for a given R.
 
@@ -195,6 +214,9 @@ class BMF:
 
         zarg = np.argsort(z) #sort just in case
         z = z[zarg]
+
+        if d_array is None:
+            d_array = self.ds_array
         
         d_array = d_array[:, np.newaxis] * CosmoParams.growthint(z)[np.newaxis, :] / CosmoParams.growthint(z[0])
     
@@ -207,7 +229,7 @@ class BMF:
         
         return niondot
     
-    def nion_delta_r_int(self, CosmoParams, d_array, z, R):
+    def nion_delta_r_int(self, CosmoParams, z, R, d_array=None):
         """
         Vectorized computation of nion over an array of overdensities d_array for a given R.
 
@@ -227,12 +249,15 @@ class BMF:
         """
 
         z.sort() #sort if not sorted
+        if d_array is None:
+            d_array = self.ds_array
         
         #reverse the inputs to make the integral easier to compute
         z_rev = z[::-1]
         Hz_rev = cosmology.Hubinvyr(CosmoParams, z_rev)
     
-        niondot_values = self.niondot_delta_r(CosmoParams, d_array, z, R)
+        niondot_values = self.niondot_delta_r(CosmoParams, z, R, d_array)
+        # niondot_values = self.niondot_delta_r(CosmoParams, d_array, z, R)
     
         integrand = -1 / (1 + z_rev) / Hz_rev * niondot_values[:, ::-1]
         nion = cumulative_trapezoid(integrand, x=z_rev, initial=0)[:, ::-1] #reverse back to increasing z order
@@ -286,7 +311,9 @@ class BMF:
 
     def converge_BMF(self, CosmoParams, ion_frac_input, max_iter):
         self.ion_frac = ion_frac_input
-        for j in range(max_iter):
+#        for j in range(max_iter):
+        iterator = range(max_iter)#trange(max_iter) if self.PRINT_SUCCESS else range(max_iter)
+        for j in iterator:
             ion_frac_prev = np.copy(self.ion_frac)
             
             self.barrier = self.compute_barrier(CosmoParams, self.ion_frac, self.zlist, self.Rs)
@@ -349,3 +376,35 @@ class BMF:
         return self.interpR(z, R, self.nion_norm_int)
     def nion_normz_int(self, z, R):
         return self.interpz(z, R, self.nion_norm_int)
+    
+    def prebarrier_xHII_int_grid(self, d, z, R):
+        """
+        Evaluate prebarrier xHII on a density field d(x),
+        at fixed redshift z and smoothing radius R.
+
+        Parameters
+        ----------
+        d: np.ndarray
+            Density/overdensity field. Can be any shape (...).
+        z: float
+            Redshift.
+        R: float
+            Smoothing radius (cMpc).
+
+        Output
+        ----------
+        values: np.ndarray
+            xHII field with the same shape as d.
+        """
+        
+        d = np.asarray(d, dtype=float)
+
+        z_arr   = np.full_like(d, float(z), dtype=float)
+        logr_arr = np.full_like(d, np.log(float(R)), dtype=float)
+
+        #stack into points (..., 3) where last axis is (delta, z, logR)
+        points = np.stack([d, z_arr, logr_arr], axis=-1)
+
+        values = self.prebarrier_xHII_int(points)
+
+        return values