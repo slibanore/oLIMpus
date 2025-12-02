fixed 21-cm map computation

oLIMpus/zeus21_local/zeus21/correlations.py
@@ -420,10 +420,12 @@ class Power_Spectra:
                                     [self.Deltasq_dxion, self.Deltasq_xaxion, self.Deltasq_Txxion, self.Deltasq_xion]]\
                                         )
 
-        self.Deltasq_T21 = np.einsum('ijk...,ijkl...->kl...', self._allbetamatrix, self._allcorrs)
-        self.Deltasq_T21 = (self.Deltasq_T21.T*T21_coefficients.T21avg_noR**2).T
+        self.Deltasq_T21_original = np.einsum('ijk...,ijkl...->kl...', self._allbetamatrix, self._allcorrs)
+        self.Deltasq_T21 = (self.Deltasq_T21_original.T*T21_coefficients.T21avg**2).T
+        self.Deltasq_T21_noR = (self.Deltasq_T21_original.T*T21_coefficients.T21avg_noR**2).T
         
-        self.Deltasq_dT21 = (np.einsum('ik...,ikl...->kl...',self._allbetas,self._allcorrs[0]).T*T21_coefficients.T21avg_noR).T
+        self.Deltasq_dT21 = (np.einsum('ik...,ikl...->kl...',self._allbetas,self._allcorrs[0]).T*T21_coefficients.T21avg).T
+        self.Deltasq_dT21_noR = (np.einsum('ik...,ikl...->kl...',self._allbetas,self._allcorrs[0]).T*T21_coefficients.T21avg_noR).T
 
 
         #Sum Linear Pop II and Pop III contributions
@@ -442,10 +444,12 @@ class Power_Spectra:
                                     [self.Deltasq_dxion_lin, self.Deltasq_xaxion_lin, self.Deltasq_Txxion_lin, self.Deltasq_xion_lin]]\
                                         )
 
-        self.Deltasq_T21_lin = np.einsum('ijk...,ijkl...->kl...', self._allbetamatrix, self._allcorrs_lin)
-        self.Deltasq_T21_lin = (self.Deltasq_T21_lin.T*T21_coefficients.T21avg_noR**2).T
+        self.Deltasq_T21_lin_original = np.einsum('ijk...,ijkl...->kl...', self._allbetamatrix, self._allcorrs_lin)
+        self.Deltasq_T21_lin = (self.Deltasq_T21_lin_original.T*T21_coefficients.T21avg**2).T
+        self.Deltasq_T21_lin_noR = (self.Deltasq_T21_lin_original.T*T21_coefficients.T21avg_noR**2).T
 
-        self.Deltasq_dT21_lin = (np.einsum('ik...,ikl...->kl...',self._allbetas,self._allcorrs_lin[0]).T*T21_coefficients.T21avg_noR).T
+        self.Deltasq_dT21_lin = (np.einsum('ik...,ikl...->kl...',self._allbetas,self._allcorrs_lin[0]).T*T21_coefficients.T21avg).T
+        self.Deltasq_dT21_lin_noR = (np.einsum('ik...,ikl...->kl...',self._allbetas,self._allcorrs_lin[0]).T*T21_coefficients.T21avg_noR).T
 #        print("Power Spectral Routine Done!")
 
 