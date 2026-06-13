f700c0598d150ee8fdebf6970bf1e4aaf6a6ce3d
slibanore <sarah.libanore@gmail.com>
2026-06-13 15:44:19 +0300

    updated input file, commented and updated Yang24 parameters from v2 of the paper

--- oLIMpus/inputs_LIM.py
+++ oLIMpus/inputs_LIM.py
@@ -7,7 +7,7 @@ BGU - June 2026
 
 
 # all the imports you will need throughtout the run 
-from zeus21 import constants, cosmology, sfrd, z21_utilities, reionization
+from zeus21 import constants, cosmology, sfrd, z21_utilities, reionization, inputs
 import astropy.constants as cu
 import astropy.units as u
 import numpy as np 
@@ -26,168 +26,226 @@ import matplotlib.cm as cm
 from matplotlib.colors import LogNorm
 import matplotlib.pyplot as plt 
 
-class LineParams_Input:
-    def __init__ (self, 
-                 LINE = 'OIII', # which line
-                 LINE_MODEL = 'SFRD', # model of the line luminosity
-                 OBSERVABLE_LIM = 'Inu', # observe intensity in Jy/sr or mK
-                 _R = 0.5, # resolution for smoothing
-                 stoch_type = 'median',
-                 sigma_LMh = 0., # stochasticity in the L-Mh relation
-                 shot_noise = False, # add shot noise to the LIM power spectrum
+from dataclasses import dataclass, field as _field, InitVar
 
-                 quadratic_lognormal = False, # use 1st or 2nd order in the SFRD and line lognormal approximation MOVE TO USER PARAMS
-                 line_dict = None
-                 ):
-        
-        self.LINE = LINE
-        self.OBSERVABLE_LIM = OBSERVABLE_LIM
-        self._R = _R
-        self.LINE_MODEL = LINE_MODEL
-        self.stoch_type = stoch_type
-        self.sigma_LMh = sigma_LMh
-        self.shot_noise = shot_noise
-
-        self.quadratic_lognormal = quadratic_lognormal
-
-        self.line_dict = line_dict
-
-
-
+@dataclass(kw_only=True)
 class Line_Parameters:
+    """
+    Parameters associated with star-forming lines and oLIMpus implementation.
 
-    "Class to pass the parameters of LIM as input"
+    Parameters
+    ----------
+    LINE: str 
+        Which line to compute. Available choices: "OIII5007" (same as "OIII"), "OIII4960", "OIII4364", "OII", "Ha", "Hb", "CII", "CO21", "CO10", "SFRD"        
+    LINE_MODEL: str 
+        How to compute the line luminosity. Can pass the name of any of the functions in luminosities_LIM.py
+    OBSERVABLE_LIM: str 
+        Whether to compute the line intensity in Jy/sr (if "Inu") or the brightness temperature in uK (if "Tnu")
+    _R: float
+        Scale below which the astrophysical model cannot be trusted (the local process is computed inside a sphere with this radius). Has to be larger than MIN_R_NONLINEAR in UserParams (see inputs.py in Zeus21)
+    shot_noise: bool
+        Whether to include shot noise in the LIM power spectrum calculation, default True
+    quadratic_rhoL: bool 
+        Whether to include the second order in the lognormal expansion of the luminosity density wrt the density field, default True 
+    stoch_type: str
+        Whether to anchor the stochastic enhancement to the mean ("mean") or on the median ("median")
+        TODO: Add burstiness option from 2605.13967
+    sigma_LMh: float = 0.   
+        Deterministic rms normal scatter in dex 
+    line_dict: dict
+        Dictionary containing all the quantities required to estimate the line luminosity (see at the bottom of this file) 
 
-    def __init__(self, LineParams_Input, User_Parameters):
+    Attributes
+    ----------
+    lambda_line: float
+        Rest-frame wavelenght of the line in Angstrom 
+    nu_line: float
+        Rest-frame frequency of the line in Hz
+    """
 
-        self.LINE = LineParams_Input.LINE # which line to use 
+    ### Default and init=False parameters
+    LINE: str = "OIII5007"
+    LINE_MODEL: str = "Yang24"
+    OBSERVABLE_LIM: str = "Inu"
+
+    _R: float = 0.5
+    shot_noise: bool = True
+    quadratic_rhoL: bool = True
+
+    stoch_type: str = "median"
+    sigma_LMh: float = 0.
+
+    line_dict: dict = _field(init=False)    
+
+    def __post_init__(self):
+        schema = {
+            "LINE": (str, {"OIII5007", "OIII", "OIII4960", "OIII4364", "OII", "Ha", "Hb", "CII", "CO21", "CO10", "SFRD"}),
+            "OBSERVABLE_LIM": (str, {"Inu", "Tnu"}),
+            "shot_noise": (bool, None),
+            "quadratic_rhoL": (bool, None),
+            "stoch_type": (str, "mean", "median"),
+        }
+        inputs.validate_fields(self, schema)
         
         if self.LINE == 'OIII4960':
-            lambda_line = 4960*u.AA 
+            self.lambda_line = 4960*u.AA 
         elif self.LINE == 'OIII5007' or self.LINE == 'OIII':
-            lambda_line = 5007*u.AA 
+            self.lambda_line = 5007*u.AA 
         elif self.LINE == 'OII':
-            lambda_line = 3727*u.AA 
+            self.lambda_line = 3727.29*u.AA 
         elif self.LINE == 'Ha':
-            lambda_line = 6563*u.AA
+            self.lambda_line = 6563*u.AA
         elif self.LINE == 'Hb':
-            lambda_line = 4861*u.AA 
+            self.lambda_line = 4861*u.AA 
+
         elif self.LINE == 'CII':
-            lambda_line = 1.58e6*u.AA 
+            self.lambda_line = 1.58e6*u.AA 
+
         elif self.LINE == 'CO21': # 2-1 transition
-            lambda_line = 1.3e7*u.AA 
-        elif self.LINE == 'CO10': # 2-1 transition
-            lambda_line = 2.6e7*u.AA 
-        elif self.LINE == 'SFRD': # 2-1 transition
-            lambda_line = 1.*u.AA 
-        self.nu_rest = (cu.c / (lambda_line)).to(u.Hz) # rest frame frequency in Hz 
+            self.lambda_line = 1.3e7*u.AA 
+        elif self.LINE == 'CO10': # 1-0 transition
+            self.lambda_line = 2.6e7*u.AA 
+        elif self.LINE == 'SFRD':
+            self.lambda_line = 1.*u.AA 
+        
+        self.nu_rest = (cu.c / (self.lambda_line)).to(u.Hz) 
 
-        self.OBSERVABLE_LIM = LineParams_Input.OBSERVABLE_LIM
+"""
+Define dictionaries containing default parameters for the models in luminosities_LIM.py
+"""
 
-        # resolution in Mpc, cannot go below the Rmin defined in Cosmo_Params               
-        if LineParams_Input._R < User_Parameters.MIN_R_NONLINEAR:
-            print('Too small R, we  use instead MIN_R_NONLINEAR')
-            self._R = User_Parameters.MIN_R_NONLINEAR
-        else:
-            self._R = LineParams_Input._R 
+########################################################
+### UV AND OPTICAL 
+########################################################
 
-        self.LINE_MODEL = LineParams_Input.LINE_MODEL 
-        self.stoch_type = LineParams_Input.stoch_type
-        self.sigma_LMh = LineParams_Input.sigma_LMh
-        if self.sigma_LMh != 0.:
-            print('You introduced stochasticity: be sure that sigma_LMh is in dex')
-        self.shot_noise = LineParams_Input.shot_noise
+# Yang24: arXiv:2409.03997v2, table 2
+# THESAN21: arXiv:2111.02411, table 2 
 
-        # !!! move to User_Params
-        self.quadratic_lognormal = LineParams_Input.quadratic_lognormal
-
-        self.line_dict = LineParams_Input.line_dict
-
-####################
-# Define parameters for some of the models included in the LIM file
-
-Yang24_OIII4960_params = {
-        'alpha': 9.46e-2,
-        'beta': 9.24e-1,
-        'N': 2.61e7,
-        'SFR1': 9.60e1,
-        }
-
+# OIII lines
 Yang24_OIII5007_params = {
-        'alpha': 9.48e-2,
-        'beta': 9.28e-1,
         'N': 7.68e7,
         'SFR1': 9.76e1,
+        'alpha': 9.48e-2,
+        'beta': 9.28e-1,
         }
 
-Yang24_OII_params = {
-        'alpha': -2.43e-1,
-        'beta': 2.49,
-        'N': 2.00e6,
-        'SFR1': 6.28e1,
+Yang24_OIII4960_params = {
+        'N': 2.61e7,
+        'SFR1': 9.60e1,
+        'alpha': 9.46e-2,
+        'beta': 9.24e-1,
         }
 
-Yang24_Ha_params = {
-        'alpha': 9.94e-3,
-        'beta': 5.25e-1,
-        'N': 4.54e7,
-        'SFR1': 3.18e1,
+Yang24_OIII4364_params = {
+        'N': 6.78e5,
+        'SFR1': 1.09e2,
+        'alpha': -9.62e-3,
+        'beta': 2.25,
         }
 
-Yang24_Hb_params = {
-        'alpha': 7.98e-3,
-        'beta': 5.61e-1,
-        'N': 1.61e7,
-        'SFR1': 1.74e1,
-        }
-
-
-THESAN21_OIII_params = {'a': 7.84,
+THESAN21_OIII_params = {
+    'a': 7.84,
     'ma': 1.24,
     'mb': 1.19,
-    'log10_SFR_b': 0.,
     'mc': 0.53, 
+    'log10_SFR_b': 0.,
     'log10_SFR_c': 0.66, 
     }
 
-THESAN21_OII_params = {'a': 7.08,
+# OII line
+Yang24_OII_params = {
+        'N': 2.00e6,
+        'SFR1': 6.28e1,
+        'alpha': -2.43e-1,
+        'beta': 2.49,
+        }
+
+THESAN21_OII_params = {
+    'a': 7.08,
     'ma': 1.11,
     'mb': 1.31,
-    'log10_SFR_b': 0.,
     'mc': 0.64, 
+    'log10_SFR_b': 0.,
     'log10_SFR_c': 0.54, 
     }
 
-THESAN21_Ha_params = {'a': 8.08,
-    'ma': 0.96,
-    'mb': 0.88,
-    'log10_SFR_b': 0.,
-    'mc': 0.45, 
-    'log10_SFR_c': 0.96, 
-    }
-
-THESAN21_Hb_params = {'a': 7.62,
-    'ma': 0.96,
-    'mb': 0.86,
-    'log10_SFR_b': 0.,
-    'mc': 0.41, 
-    'log10_SFR_c': 0.96, 
-    }
-
-Lagache18_CII_params = {'alpha_SFR_0': 1.4-0.07*10,
-        'beta_SFR_0': 7.1-0.07*10,
-        'alpha_SFR': 0.,
-        'beta_SFR': 0.,
+# Ha line
+Yang24_Ha_params = {
+        'N': 4.28e7,
+        'SFR1': 4.42e1,
+        'alpha': -4.03e-3,
+        'beta': 5.84e-1,
         }
 
-Yang21_CO21_params = {
-    'A':1.
-}
+THESAN21_Ha_params = {
+    'a': 8.08,
+    'ma': 0.96,
+    'mb': 0.88,
+    'mc': 0.45, 
+    'log10_SFR_b': 0.,
+    'log10_SFR_c': 0.96, 
+    }
 
+# Hb line
+Yang24_Hb_params = {
+        'N': 1.63e7,
+        'SFR1': 1.79e1,
+        'alpha': 1.78e-2,
+        'beta': 5.77e-1,
+        }
+
+THESAN21_Hb_params = {
+    'a': 7.62,
+    'ma': 0.96,
+    'mb': 0.86,
+    'mc': 0.41, 
+    'log10_SFR_b': 0.,
+    'log10_SFR_c': 0.96, 
+    }
+
+########################################################
+### INFRARED
+########################################################
+
+# Lagache18: 1711.00798 anchored at z=10
+
+# CII line
+Lagache18_CII_params ={
+    'alpha_SFR_0': 1.4-0.07*10,
+    'beta_SFR_0': 7.1-0.07*10,
+    'alpha_SFR': 0.,
+    'beta_SFR': 0.,
+    }
+
+########################################################
+### SUB-MM CO
+########################################################
+
+# Li16: arXiv:
+# Yang21: arXiv:2108.07716, scale up/down the empirical fit
+
+# CO 2-1 transition
 Li16_C021_params = {
     'alpha':1.11,
     'beta':0.6,
     'dMF':1.,
     'L0':4.9e-5,
     'sigma_SFR':0.3*u.dex
+}
+
+Yang21_CO21_params = {
+    'A':1.
+}
+
+# CO 1-0 transition
+Li16_C010_params = {
+    'alpha':1.11,
+    'beta':0.6,
+    'dMF':1.,
+    'L0':4.9e-5,
+    'sigma_SFR':0.3*u.dex
+}
+
+Yang21_CO10_params = {
+    'A':1.
 }
