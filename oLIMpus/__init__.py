7b2d606dcb16eaf5193bbdfb5a758cd1d03cc731
slibanore <sarah.libanore@gmail.com>
2026-06-15 12:58:36 +0300

    updated analysis.py wrapper

--- oLIMpus/__init__.py
+++ oLIMpus/__init__.py
@@ -9,3 +9,4 @@ from .maps_LIM import *
 
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning) #to silence unnecessary warning in mcfit
+warnings.filterwarnings("ignore", category=RuntimeWarning) 
--- oLIMpus/analysis.py
+++ oLIMpus/analysis.py
@@ -1,6 +1,9 @@
 from oLIMpus import * 
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+from zeus21.inputs import User_Parameters, Cosmo_Parameters, Astro_Parameters
+from zeus21 import T21coefficients, correlations
+from oLIMpus.inputs_LIM import Line_Parameters
 from copy import copy 
 
 plt.rcParams.update({"text.usetex": True, "font.family": "Times new roman"}) # Use latex fonts
@@ -22,7 +25,7 @@ plt.rcParams.update({
 })
 
 
-CosmoParams_input_fid = dict(
+CosmoParams_inputs = dict(
         omegab= 0.0223828, 
         omegac = 0.1201075, 
         h_fid = 0.67810, 
@@ -31,11 +34,9 @@ CosmoParams_input_fid = dict(
         tau_fid = 0.05430842, 
         HMF_CHOICE= "ST",
         Flag_emulate_21cmfast = False,
-        _Mhmin = 1e5,
-        _Mhmax = 1e14
         )
 
-AstroParams_input_fid = dict(
+AstroParams_inputs = dict(
         # values from 2306.09403
         # alphastar = 0.61,
         # betastar = -1.91,
@@ -45,18 +46,15 @@ AstroParams_input_fid = dict(
 
         # # we fix these values to get fduty == 1
         # Mturn_fixed = 1e-10,
-
-        astromodel = 0,
-        accretion_model = 0,
+        accretion_model = 'exp',
         alphastar = 0.5,
         betastar = -0.5,
         epsstar = 0.1,
         Mc = 3e11,
         Mturn_fixed = None,
         dlog10epsstardz = 0.0,
-        quadratic_SFRD_lognormal = False, # Sarah Libanore
+        quadratic_SFRD_lognormal = True, 
 
-#############################
         fesc10 = 0.1,
         alphaesc = 0., 
         L40_xray = 3.0,
@@ -64,30 +62,6 @@ AstroParams_input_fid = dict(
         alpha_xray = -1.0,
         Emax_xray_norm=2000,
 
-        Nalpha_lyA_II = 9690,
-        Nalpha_lyA_III = 17900,
-
-        FLAG_MTURN_SHARP= False,
-
-        C0dust = 4.43,
-        C1dust = 1.99,
-        sigmaUV=0.5,
-
-        USE_POPIII = False,
-        alphastar_III = 0, 
-        betastar_III = 0,
-        fstar_III = 10**(-2.5),
-        Mc_III = 1e7,
-        dlog10epsstardz_III = 0.0,
-        fesc7_III = 10**(-1.35),
-        alphaesc_III = -0.3,
-        L40_xray_III = 3.0,
-        alpha_xray_III = -1.0,        
-        USE_LW_FEEDBACK = False,
-        A_LW = 2.0,
-        beta_LW = 0.6,
-        A_vcb = 1.0,
-        beta_vcb = 1.8,
         )
 
 
@@ -95,7 +69,7 @@ AstroParams_input_fid = dict(
 "Class to store the quantities needed in the LIM computation and analysis, define in the input list the ones that you want to vary while the others are fiducial"
 class run_oLIMpus:
 
-    def __init__(self, LINE, LINE_MODEL = 'Yang24', _R = 1., shot_noise= False, quadratic_lognormal=True, sigma_LMh = 0., astromodel=0, ZMIN = 5., RSD_MODE = 0, sigma_FoG=0.,\
+    def __init__(self, LINE, LINE_MODEL = 'Yang24', _R = 1., shot_noise= False, quadratic_lognormal=True, sigma_LMh = 0., RSD_MODE = 0, SIGMA_FOG=0.,\
         alphastar = 0.5,
         betastar = -0.5,
         epsstar = 0.1,
@@ -108,17 +82,21 @@ class run_oLIMpus:
 
         self.UP = User_Parameters(
             precisionboost= 1.0, 
-            FLAG_FORCE_LINEAR_CF= 0, 
+            FLAG_FORCE_LINEAR_CF=  False, 
             MIN_R_NONLINEAR= 0.5, 
             MAX_R_NONLINEAR= 200.0,
             FLAG_DO_DENS_NL= False, 
             FLAG_WF_ITERATIVE= True,
             )
 
-        self.CP, ClassyC, self.zeus_corr, self.HMFcl =  cosmo_wrapper(self.UP, Cosmo_Parameters_Input(**CosmoParams_input_fid))
+        print('Setting Cosmology...')
+        self.CP = Cosmo_Parameters(UserParams=self.UP, **CosmoParams_inputs)
+        
+        print('...Defining HMF...')
+        self.HMFcl = cosmology.HMF_interpolator(UserParams=self.UP,CosmoParams=self.CP)
 
-        AstroParams_input = copy(AstroParams_input_fid)
-        AstroParams_input['astromodel'] = astromodel
+        print('...Setting Astrophysics')
+        AstroParams_input = copy(AstroParams_inputs)
         AstroParams_input['quadratic_SFRD_lognormal'] = quadratic_lognormal
         AstroParams_input['alphastar'] = alphastar
         AstroParams_input['betastar'] = betastar
@@ -127,27 +105,29 @@ class run_oLIMpus:
         AstroParams_input['Mc'] = Mc
         AstroParams_input['dlog10epsstardz'] = dlog10epsstardz
         AstroParams_input['fesc10'] = fesc
-        self.AP = zeus21.Astro_Parameters(self.UP, self.CP, **AstroParams_input)
 
-        LineParams_Input_val = LineParams_Input(
+        self.AP = Astro_Parameters(CosmoParams= self.CP, **AstroParams_input)
+
+        print('...Setting Line properties...')
+        self.LP = Line_Parameters(
             LINE = LINE, # which line
             LINE_MODEL = LINE_MODEL, # model of the line luminosity
             OBSERVABLE_LIM = LIM_observable, # observe intensity in Jy/sr or mK
             _R = _R, # resolution for smoothing
             sigma_LMh = sigma_LMh, # stochasticity in the L-SFR relation
             shot_noise = shot_noise, # add shot noise to the power spectrum
-            quadratic_lognormal = quadratic_lognormal, # use 1st or 2nd order in the SFRD and line lognormal approximation MOVE TO USER PARAMS
+            quadratic_rhoL = quadratic_lognormal, # use 1st or 2nd order in the SFRD and line lognormal approximation MOVE TO USER PARAMS
             line_dict= line_dict # parameters that enter the L-SFR or L-Mh relation
         )
-
-        self.LP = Line_Parameters(LineParams_Input_val,self.UP)
         
-        self.LIM_coeff = get_LIM_coefficients(self.LP, self.AP, self.CP, self.HMFcl, self.UP, ZMIN)
+        print('...Running Line...')
+        self.LIMcoeff = coefficients_LIM.get_LIM_coefficients(self.UP,self.CP,self.AP,self.LP,self.HMFcl)
 
-        self.LIM_corr = Correlations_LIM(self.LP, self.CP, ClassyC)
+        self.LIMpk = correlations_LIM.Power_Spectra_LIM(self.UP,self.CP,self.LP,self.LIMcoeff,RSD_MODE=RSD_MODE,SIGMA_FOG=SIGMA_FOG)
 
-        self.LIM_pk = Power_Spectra_LIM(self.LIM_corr, self.LIM_coeff, self.LP, self.CP, self.UP, RSD_MODE,sigma_FoG=sigma_FoG)
+        print('...Running T21...')
+        self.T21coeff = T21coefficients.get_T21_coefficients(self.UP,self.CP,self.AP,self.HMFcl)
 
-        self.zeus_coeff = get_T21_coefficients(self.UP, self.CP, ClassyC, self.AP, self.HMFcl, ZMIN)
+        self.T21pk = correlations.Power_Spectra(self.UP,self.CP,self.AP,self.T21coeff, RSD_MODE=RSD_MODE)
 
-        self.zeus_pk = Power_Spectra(self.UP, self.CP, self.AP, ClassyC, self.zeus_corr, self.zeus_coeff, RSD_MODE)
+        print('...Done!')
